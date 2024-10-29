863d1a0c5e06f09df8dfd9e61ac466f862fb2e45
DHEERAJ <86879344+dheeraj7-13@users.noreply.github.com>
2024-10-29 13:35:33 +0530

    Add files via upload

---
+++ calculator/CalculatorLambdaImpl.java
@@ -0,0 +1,26 @@
+import java.util.*;
+interface CalculatorLambda 
+{ // fucntional interface
+ 
+    int op(int a,int b);
+} 
+
+class CalculatorLambdaImpl
+{
+    public static void main(String[] args) 
+    {
+        //instantiate 
+        CalculatorLambda add=(a,b)-> a+b;
+        CalculatorLambda sub=(a,b)-> a-b;
+        CalculatorLambda mul=(a,b)-> a*b;
+        CalculatorLambda div=(a,b)-> a/b;
+
+        //call the funciton
+        System.out.println("Addition: "+add.op(10,20));
+        System.out.println("Subtraction: "+sub.op(10,20));
+        System.out.println("Multiplication: "+mul.op(10,20));
+        System.out.println("Division: "+div.op(10,20));
+        
+
+    }
+}
