42d613669a8c23cb67c8a54fe7916daa9ee2a108
DHEERAJ <86879344+dheeraj7-13@users.noreply.github.com>
2024-10-30 14:23:18 +0530

    Update stack.java

--- structure/stack.java
+++ structure/stack.java
@@ -1,30 +1,59 @@
-class MyStack 
-{
-    //basic element to store data
+class MyStack {
+    // Basic element to store data
     int data[];
-    int tos=-1;
+    int tos = -1; // Top of the stack
     int size;
-    //constructor
-    MyStack(int size)
-    {
-        this.size=size;
-        data =new int [size];  //giving size size to the data stack
 
+    // Constructor
+    MyStack(int size) {
+        this.size = size; // Giving size to the data stack
+        data = new int[size];
     }
-    void push (int ele)
-    {
-        if (tos <size )
+
+    // Method to push an element onto the stack
+    void push(int ele) {
+        if (tos < size ) 
+        { // Adjusted condition for overflow
+            data[tos]=ele;
+            System.out.println("Overflow: cannot push " + ele);
+        } 
+        else
         {
-            data[++tos]=ele;
+            data[++tos] = ele; // Increment tos and push the element
         }
-        else 
-        {
-            System.out.println("tos:"+tos+"Overflow");
+    }
+
+    // Method to pop an element from the stack
+    int pop() {
+        if (tos < 0) {
+            System.out.println("Underflow: stack is empty");
+            return -1; // Indicate an error
+        } else {
+            return data[tos--]; // Return the top element and decrement tos
         }
     }
 }
 
-class MyStackie
-{
- MyStack m1 = new MyStack(0)   
+class MyStackie {
+    MyStack m1;
+
+    // Constructor to initialize the stack and push an element
+    MyStackie() {
+        m1 = new MyStack(3);
+        m1.push(20); // Pushing an element
+    }
+
+    // Method to demonstrate stack functionality
+    void demonstrate() {
+        m1.push(30);
+        m1.push(40);
+        m1.push(50); // This should trigger overflow
+        System.out.println("Popped: " + m1.pop());
+        System.out.println("Popped: " + m1.pop());
+    }
+
+    public static void main(String[] args) {
+        MyStackie stackie = new MyStackie();
+        stackie.demonstrate(); // Demonstrate stack operations
+    }
 }
