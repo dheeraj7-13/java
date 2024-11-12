6ec9e6a37118141ea3f395e5d90a78d71c0a535c
DHEERAJ <86879344+dheeraj7-13@users.noreply.github.com>
2024-11-13 00:37:52 +0530

    Update pushoverflow.java

--- structure/pushoverflow.java
+++ structure/pushoverflow.java
@@ -1,65 +1,60 @@
 import java.util.*;
 class MyStack<T>
 {
-        //instance variable to store data
-        ArrayList<T> data;
-        //represents size of stack
+        ArrayList<T> data;     /*ArrayList<T>    type data */
         int size;
-        //reprsents the pointer
         int tos=-1;
 
         MyStack(int size)
         {
                 this.size=size;
-                data=new ArrayList<>(size);
+                data=new ArrayList<>();
         }
-
-        //push element on stack
-        void push(T ele)
+        //push
+        void push (T ele  )  //setter
         {
-                //increment tos
                 tos++;
                 if(tos<size)
                 {
                         data.add(tos,ele);
-                        System.out.println("Pushed!");
+                        System.out.println("Pushed!"+ele);
                 }
                 else
                 {
                         System.out.println("Overflow!");
                 }
+                
         }
-
-        //pop element
-        T pop()
+        T pop() //getter
         {
                 tos--;
-                if(tos>=0)
+                if(tos>=0)       
                 {
                         return data.remove(tos);
                 }
                 else
                 {
-                        System.out.println("Underflow!");
+                        System.out.println("underflow!!");
                         return null;
                 }
         }
 
 }
 
-class GenericStackImpl
-{
-        public static void main(String[] args)
-        {
-                MyStack<Integer> s1=new MyStack<>(3);
 
+public class GenericStackImpl {
+
+        public static void main(String[] args) 
+        {
+                MyStack<Integer> s1 = new MyStack<>(3);
                 for(int i=1;i<=4;i++)
                 {
                         s1.push(i*10);
                 }
                 for(int i=0;i<=3;i++)
                 {
-                        System.out.println("Popped: "+s1.pop());
+                        System.out.println("popped:"+s1.pop());
                 }
+
         }
 }
